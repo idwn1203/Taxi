2306c264dec34b992a2b3132efdb7018a27ef7cd
JJunney <idwn1203@gmail.com>
2018-07-06 02:33:17 +0900

    make menu

--- app/src/main/java/com/example/iic_common/taxiapp/MainActivity.java
+++ app/src/main/java/com/example/iic_common/taxiapp/MainActivity.java
@@ -4,6 +4,8 @@ package com.example.iic_common.taxiapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,7 +16,6 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     //activity_main
     Button login;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +29,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
     }
 }
--- app/src/main/java/com/example/iic_common/taxiapp/SendActivity.java
+++ app/src/main/java/com/example/iic_common/taxiapp/SendActivity.java
@@ -3,6 +3,9 @@ package com.example.iic_common.taxiapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,17 +13,26 @@ import android.widget.Button;
  * Created by iic_common on 2018-07-05.
  */
 
-public class SendActivity extends Activity {
+public class SendActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.send);
-            Button logout = (Button)findViewById(R.id.start);
-            logout.setOnClickListener(new View.OnClickListener(){
-                public void onClick(View v){
-                   finish();
-                }
-            });
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.send);
+        setTitle("Taxi");
+        Button logout = (Button) findViewById(R.id.start);
+        logout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater mInflater = getMenuInflater();
+        mInflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
